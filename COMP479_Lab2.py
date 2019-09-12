284088bff9354ac1e1f8c510fb67728beb2f36f4
Michel Maroun <michoma94@outlook.com>
2019-09-12 14:06:08 -0400

    Without the generated files. Run the code and see for yourself!

--- COMP479_Lab2.py
+++ COMP479_Lab2.py
@@ -11,27 +11,30 @@ cpt = sum([len(files) for r, d, files in os.walk(str(directory)+"/reuters")])
 #Get all reuters tags from all files
 
 for i in range(cpt):
+    #Search for all reuters articles
     file_name = 'reut2-' + str(f'{i:03}') + '.sgm'
     reuters_file = open(directory + "/reuters/" + file_name).read()
     file_table = file_name.split(".")[0] + "_table.txt"
+    if path.exists(file_table):
+        print(file_table + " already exists in directory!")
+    else:    
+        soup = BeautifulSoup(reuters_file, 'html.parser')
+        reuters_tag = soup.find_all('reuters')
 
-    soup = BeautifulSoup(reuters_file, 'html.parser')
-    reuters_tag = soup.find_all('reuters')
-
-    all_places = open('all-places-strings.lc.txt').read()
-    list_places = []
-
-    for reuter in reuters_tag:
-        article_id = reuter.get('newid')
-        row = "Article newid="+str(article_id) + ": " 
-        #check for places in reuter article
-        for place in all_places.split("\n"):
-            if place in reuter.get_text():
-                row += place + " | "
-        #write to file
-        row += "\n"
-        table = open(file_table, "a")
-        table.write(row)
+        all_places = open('all-places-strings.lc.txt').read()
+        list_places = []
+        #loop through articles 
+        for reuter in reuters_tag:
+            article_id = reuter.get('newid')
+            row = "Article newid="+str(article_id) + ": " 
+            #check for places in reuter article
+            for place in all_places.split("\n"):
+                if place in reuter.get_text():
+                    row += place + " | "
+            #write to file
+            row += "\n"
+            table = open(file_table, "a")
+            table.write(row)
 
 # for place in all_places.split("\n"):
 #     for reuter in reuters_tag:
