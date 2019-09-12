1e876f31cbefcd5bd034ed9ee571151f3a74ad42
Michel Maroun <michoma94@outlook.com>
2019-09-12 13:56:46 -0400

    searches places in all reuters files!

--- COMP479_Lab2.py
+++ COMP479_Lab2.py
@@ -9,30 +9,29 @@ cpt = sum([len(files) for r, d, files in os.walk(str(directory)+"/reuters")])
 
 
 #Get all reuters tags from all files
-file_name = 'reut2-'
 
-# for i in range(cpt):
-#     file_name + f'{i:03}' + '.sgm'
-#     reuters_file = open(file_name).read()
-reuters_file = open('reut2-000.sgm').read()
+for i in range(cpt):
+    file_name = 'reut2-' + str(f'{i:03}') + '.sgm'
+    reuters_file = open(directory + "/reuters/" + file_name).read()
+    file_table = file_name.split(".")[0] + "_table.txt"
 
-soup = BeautifulSoup(reuters_file, 'html.parser')
-reuters_tag = soup.find_all('reuters')
+    soup = BeautifulSoup(reuters_file, 'html.parser')
+    reuters_tag = soup.find_all('reuters')
 
-all_places = open('all-places-strings.lc.txt').read()
-list_places = []
+    all_places = open('all-places-strings.lc.txt').read()
+    list_places = []
 
-for reuter in reuters_tag:
-    article_id = reuter.get('newid')
-    row = "Article newid="+str(article_id) + ": " 
-    #check for places in reuter article
-    for place in all_places.split("\n"):
-        if place in reuter.get_text():
-            row += place + " | "
-    #write to file
-    row += "\n"
-    file_table = open("reut2-001_table.txt", "a")
-    file_table.write(row)
+    for reuter in reuters_tag:
+        article_id = reuter.get('newid')
+        row = "Article newid="+str(article_id) + ": " 
+        #check for places in reuter article
+        for place in all_places.split("\n"):
+            if place in reuter.get_text():
+                row += place + " | "
+        #write to file
+        row += "\n"
+        table = open(file_table, "a")
+        table.write(row)
 
 # for place in all_places.split("\n"):
 #     for reuter in reuters_tag:
